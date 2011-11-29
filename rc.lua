1acefd6ecf8d7bd7f458468f1d44ca944faface2
Vlad Gusev <vlad@s10.in>
2011-11-29 20:02:10 +0400

    * Add battery widget

--- rc.lua
+++ rc.lua
@@ -141,6 +141,53 @@ dbus.add_signal("ru.gentoo.kbdd", function(...)
 )
 
 -- {{{ Wibox
+
+-- Function for battery widget
+function batteryInfo(adapter)
+    spacer = " "
+    local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")
+    local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
+    local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
+    local cur = fcur:read()
+    local cap = fcap:read()
+    local sta = fsta:read()
+    local battery = math.floor(cur * 100 / cap)
+    if sta:match("Charging") then
+        dir = "^"
+        battery = "A/C ("..battery..")"
+    elseif sta:match("Discharging") then
+        dir = "v"
+        if tonumber(battery) > 25 and tonumber(battery) < 75 then
+            battery = battery
+        elseif tonumber(battery) < 25 then
+            if tonumber(battery) < 10 then
+                naughty.notify({ title      = "Battery Warning"
+                               , text       = "Battery low!"..spacer..battery.."%"..spacer.."left!"
+                               , timeout    = 5
+                               , position   = "top_right"
+                               , fg         = beautiful.fg_focus
+                               , bg         = beautiful.bg_focus
+                               })
+            end
+            battery = battery
+        else
+            battery = battery
+        end
+    else
+        dir = " "
+        battery = "A/C"
+    end
+    batterywidget.text = spacer..dir..battery..dir..spacer
+    fcur:close()
+    fcap:close()
+    fsta:close()
+end
+awful.hooks.timer.register(20, function()
+    batteryInfo("BAT0")
+end)
+
+batterywidget = widget({type = "textbox", name = "batterywidget", align = "right" })
+
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
 
@@ -291,6 +338,7 @@ for s = 1, screen.count() do
         mylayoutbox[s],
         binaryclock.widget,
         kbdwidget,
+        batterywidget,
         -- mytextclock,
         -- s == 1 and mysystray or nil,
         mytasklist[s],
